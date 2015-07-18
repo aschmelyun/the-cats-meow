dbb9c5904624b3b08b4fc2f9676501b68b8d857a
Andrew Schmelyun <me@aschmelyun.com>
2015-07-18 19:43:16 -0400

    Bullet firing delay added

--- states/level.js
+++ states/level.js
@@ -1,3 +1,6 @@
+var BULLET_DELAY = 500;
+var BULLET_TIME = 500;
+
 var LevelState = function(game) {};
 
 LevelState.prototype = {
@@ -6,9 +9,8 @@ LevelState.prototype = {
 	},
 
 	create: function() {
-		this.spaceKey = this.game.input.keyboard
+		this.fireButton = this.game.input.keyboard
             .addKey(Phaser.Keyboard.SPACEBAR);
-        this.spaceKey.onDown.add(this.fireBullet, this);
 
         this.shootSound = this.game.add.audio('shoot');
         this.background_1 = this.game.add.sprite(0, 0, 'background');
@@ -22,11 +24,18 @@ LevelState.prototype = {
 	update: function() {
 		this.moveBackground(this.background_1);
         this.moveBackground(this.background_2);
+
+        if(this.fireButton.isDown) {
+            this.fireBullet();
+        }
 	},
 
     fireBullet: function() {
         // fire a bullet, play a sound.
-        this.shootSound.play();
+        if (this.game.time.now > BULLET_TIME) {
+            this.shootSound.play();
+            BULLET_TIME = this.game.time.now + BULLET_DELAY;
+        }
     },
 
     moveBackground: function(background) {
